e33d1521cdaea3a2f0bcba32cb2981893f31e2e5
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2022-06-13 19:54:43 +0200

    Add simple array example

--- array.cpp
+++ array.cpp
@@ -1,5 +1,13 @@
 #include "helpers.hpp"
+#include <array>
+
+using pp::displayRange;
+using pp::br;
 int main()
 {
+    std::array<int, 3> a1 {1,2,3};
+    a1.fill(7);
+    displayRange(a1.begin(),a1.end());
+    br("//EOF/");
     return 0;
 }
