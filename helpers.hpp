015bd632af7eeb954b0d73e8070fd6e88c7f7271
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2022-06-13 19:40:05 +0200

    Add additional vector<int> examples and modify helpers

--- helpers.hpp
+++ helpers.hpp
@@ -1,9 +1,26 @@
 #ifndef HELPERS
 #define HELPERS
+
+#ifndef IOSTREAM
+#define IOSTREAM
+#include <iostream>
+#endif
+
 namespace pp {
-void br()
+void br(std::string desc="")
 {
-    std::cout << std::endl;
+    std::cout << desc <<std::endl;
+    
 }
+
+template <typename FIT>
+void displayRange(FIT first, FIT last)
+{
+    while (first != last)
+    {
+        std::cout << *first++ <<", ";
+    }
+}
+
 }
 #endif
--- vector.cpp
+++ vector.cpp
@@ -7,20 +7,16 @@ void c_function(int* CArrayPointer, size_t size)
     //c_function(vector<int>.data(), vector<int>.size())
 }
 
-template <typename FIT>
-void displayRange(FIT first, FIT last)
-{
-    while (first != last)
-    {
-        std::cout << *first++ <<", ";
-    }
-}
 
 using pp::br;
+using pp::displayRange;
 int main()
 {
-    //////////////////////////////////////////////////////////
+    br("////////////////////////////////////////////////////////////////////////////");
     std::vector<int> primes {2,3,5,7,11,13};
+    br("Primes examples with: ");
+    displayRange(primes.begin(),primes.end());
+    br();
 
     int product = 1;
     int sum = 0;
@@ -34,26 +30,61 @@ int main()
  
     std::cout << "Front: " << primes.front() << std::endl << "Back: " << primes.back() <<std::endl;
     std::cout << "Size: " << primes.size() << std::endl;
-    br();
+    
+    br("Insert 17 at primes.end()");
     auto it = primes.insert(primes.end(), 17);
     ////auto it = primes.emplace(primes.begin(),17);
     std::cout << *it;
-    br();
-
-    br();
-    displayRange(primes.rbegin(), primes.rend());//use Forward Iterator to iterate from end to start
-    br();
+    br("<-*it showed this");
+    br("Primes now:");
+    displayRange(primes.rbegin(), primes.rend());  br("from .rbegin() to .rend()");
     displayRange(primes.begin(), primes.end());
-    br();
+    br("from .begin() to .end()");
 
-    ////////////////////////////////////////////////////////////////////////////
-    std::vector<int> v1 {1,2,3,5,6,7,8,9};
-
+    br("////////////////////////////////////////////////////////////////////////////");
+    
+    br("Examples with vector:");
+    std::vector<int> v1 {1,2,3,4,5,6,7,8,9};
     //#include <iterator>
     //OutputIt copy(InputIt first, InputIt last, OutputIt destination_first)
     std::copy(v1.begin(),v1.end(),
-            std::ostream_iterator<int>(std::cout,"-"));
+            std::ostream_iterator<int>(std::cout,","));
+    br("//copy to ostream_iterator used");
+    
+    br(".erase");
+    v1.erase(v1.begin());
+    displayRange(v1.begin(),v1.end());
+    v1.emplace(v1.begin(),1);
+    br();
+    displayRange(v1.begin(),v1.end());
 
+
+    br(".pop_back (deleted last, no return)");
+    v1.pop_back(); //-> delete last, no return
+    v1.emplace_back(9);
+    br("pop_back deleted last element (9) so emplace_back added 9 manualy");
+    displayRange(v1.begin(),v1.end());
+
+    br("After .resize(6) and resize(9) -> data lost");
+    v1.resize(6);
+    v1.resize(9);
+    displayRange(v1.begin(), v1.end());
+    v1.at(6) =7;
+    v1.at(7) =8;
+    v1.at(8) =9;
+    displayRange(v1.begin(), v1.end());
+    br();
+
+
+    br("v1 and v2");
+    std::vector<int> v2 {0};
+    displayRange(v1.begin(),v1.end()); br("<-v1");
+    displayRange(v2.begin(),v2.end()); br("<-v2");
+    v1.swap(v2); br("after v1.swap(v2);");
+    displayRange(v1.begin(),v1.end()); br("<-v1");
+    displayRange(v2.begin(),v2.end()); br("<-v2");
+    
+    
  
     return 0;
 }
