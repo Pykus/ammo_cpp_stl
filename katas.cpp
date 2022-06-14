90a9aa0a606e857456b5782119313b393e9064c8
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2022-06-14 16:52:02 +0200

    Add working Task1_PowersOfTwo

--- katas.cpp
+++ katas.cpp
@@ -6,39 +6,37 @@
 
 using pp::displayRange;
 using pp::br;
-
 TEST(HelloTest, BasicAssertions)
 {
     EXPECT_STRNE("hello","world");
-    std::cout<<"hello?";
     EXPECT_EQ(7*6,42);
 }
-
 
 std::vector<uint64_t> powers_of_two(int n) 
 {
  /* Task-1 powers of 2
- powers_of_two(0) ->  std::vector<uint64_t>{1}
- powers_of_two(2) ->  std::vector<uint64_t>{1, 2, 4}
  powers_of_two(4) ->  std::vector<uint64_t>{1, 2, 4, 8, 16}
- powers_of_two(10) ->  std::vector<uint64_t>{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
-*/
+ powers_of_two(10) ->  std::vector<uint64_t>{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}*/
     std::vector<uint64_t> result {};
     for(auto i = 0; i <= n; i++)
     {         
         result.emplace_back(pow(2,i));
     }
     return result;
-   /*main() Task-1 solution
-    std::vector<uint64_t> v1;
-    v1 = powers_of_two(4);
-    displayRange(v1.begin(), v1.end());
-    s/*/
- 
 }
+
 
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
+TEST(Task1_PowersOfTwo, GivenANumberReturnsPowersOfTwoUpToThisNumberAsVector)
+{
+    EXPECT_EQ(std::vector<uint64_t>{1},powers_of_two(0));
+    EXPECT_EQ((std::vector<uint64_t>{1,2}),powers_of_two(1));
+    EXPECT_EQ((std::vector<uint64_t>{1,2,4}),powers_of_two(2));
+    EXPECT_EQ((std::vector<uint64_t>{1,2,4,8}),powers_of_two(3));
+}
+
+
