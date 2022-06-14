015cf2e1097b1c0139b9762467a9227186359c9c
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2022-06-14 15:49:48 +0200

    Modify CMakeList

--- array.cpp
+++ array.cpp
@@ -8,6 +8,16 @@ int main()
     std::array<int, 3> a1 {1,2,3};
     a1.fill(7);
     displayRange(a1.begin(),a1.end());
+    br();
+    //.empty()   .size()   .max_size()
+
+    std::array<int,3> a2 {5,4,3};
+    a1.swap(a2);
+    std::printf("a1:");
+    displayRange(a1.begin(),a1.end());
+    br();
+    
     br("//EOF/");
     return 0;
+
 }
