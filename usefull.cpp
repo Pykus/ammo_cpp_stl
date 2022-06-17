bc359613170b7bcccfebf48c7776385cc5df4e89
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2022-06-17 11:58:11 +0200

    Add more examples

--- usefull.cpp
+++ usefull.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <tuple>
 struct P
 {           
     int x;
@@ -5,3 +7,15 @@ struct P
 };
 P p = {1,2};//p.x p.y
 auto[x,y] = p; //x y
+
+struct P2
+{
+    int x;
+    int y;
+    std::string s;
+};
+auto func() -> P2 {
+    //std::tuple<int,int,std::string> t {1,2, std::string("abc")};
+    return {1,2, std::string("abc")}; //t;
+}
+auto[x,y,z] = func();
